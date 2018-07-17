8a00746c73ea09d70b267340ae12d4489261cfdc
logunlaja26 <logunlaja26@gmail.com>
2018-07-17 10:13:12 -0500

    Create actors.py

---
+++ actors.py
@@ -0,0 +1,6 @@
+class Characters:
+    def __init__(self, opportunities, emotions, health):
+        self.opportunities = opportunities
+        self.emotions = emotions
+        self.health = health
+        
